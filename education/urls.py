0f7500bfda8986afe3123f7d6b928728b59ba0c2
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-07 22:33:00 +0300

    созданы generics для Lesson и прописаны urls

--- education/urls.py
+++ education/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
-from education.views import CourseViewSet
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = EducationConfig.name
 
@@ -9,5 +11,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson_create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson_list/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk/>', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson_update/<int:pk/>', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson_delete/<int:pk/>', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
