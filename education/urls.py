926fb93c4cb82ce107e98365ed31a9965ed0030a
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-07 22:34:02 +0300

    исправлена опечатка

--- education/urls.py
+++ education/urls.py
@@ -13,7 +13,7 @@ router.register(r'courses', CourseViewSet, basename='courses')
 urlpatterns = [
     path('lesson_create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lesson_list/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/<int:pk/>', LessonRetrieveAPIView.as_view(), name='lesson_get'),
-    path('lesson_update/<int:pk/>', LessonUpdateAPIView.as_view(), name='lesson_update'),
-    path('lesson_delete/<int:pk/>', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson_update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson_delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
