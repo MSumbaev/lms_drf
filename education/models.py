58d9b5d82621e557e5ee9d181a7e51d3925781b2
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-07 21:52:44 +0300

    созданы модели User, Course, Lesson

--- education/models.py
+++ education/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    preview = models.ImageField(upload_to='education/course/', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    preview = models.ImageField(upload_to='education/lesson/', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    link = models.URLField(max_length=250, verbose_name='Ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    phone = models.CharField(max_length=30, verbose_name='Телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='Город', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
