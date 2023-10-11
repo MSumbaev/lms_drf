8273bad86e5f375c6ef6edca3d90e5b56fc05796
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-11 23:20:57 +0300

    для CourseSerializer добавлено поле вывода уроков lessons

--- education/serializers.py
+++ education/serializers.py
@@ -1,18 +1,24 @@
 from rest_framework import serializers
 
-from education.models import Course, Lesson
+from education.models import Course, Lesson, Payments
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.IntegerField(source='lesson_set.all.count')
+    lessons = LessonSerializer(source='lesson_set.all', many=True)
 
     class Meta:
         model = Course
         fields = '__all__'
 
 
-class LessonSerializer(serializers.ModelSerializer):
-
+class PaymentsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lesson
+        model = Payments
         fields = '__all__'
