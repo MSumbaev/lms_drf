07c84c682457b33db35b58fe4ba6fe67c5715331
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-19 21:25:44 +0300

    создан validators.py в котором написан класс LinkValidator для проверки ссылки на youtube.com

---
+++ education/validators.py
@@ -0,0 +1,20 @@
+import re
+
+from rest_framework.exceptions import ValidationError
+
+
+class LinkValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        value_tmp = dict(value).get(self.field)
+        link_pattern = r'https?://\S+|www\.\S+'
+        youtube_pattern = r'(?:https?://)?(?:www\.)?youtube\.com'
+
+        links = re.findall(link_pattern, value_tmp)
+
+        for link in links:
+            if not bool(re.match(youtube_pattern, link)):
+                raise ValidationError('Можете добавить ссылку только на "youtube.com"!')
