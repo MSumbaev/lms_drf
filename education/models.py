279587d3253622a973d680248edb5c871b6cc3a7
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-23 20:54:10 +0300

    создана модель Subscription

--- education/models.py
+++ education/models.py
@@ -62,3 +62,15 @@ class Payments(models.Model):
     class Meta:
         verbose_name = 'Платеж'
         verbose_name_plural = 'Платежи'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return f'{self.user} - {self.course}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
