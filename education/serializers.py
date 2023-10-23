7ca910a08386d55f149a9a5189e9fb2bac949c95
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-23 20:56:09 +0300

    создан сериализатор SubscriptionSerializer;добавлены поле is_subscribed и метод get_is_subscribed

--- education/serializers.py
+++ education/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 
-from education.models import Course, Lesson, Payments
+from education.models import Course, Lesson, Payments, Subscription
 from education.validators import LinkValidator
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -15,13 +21,24 @@ class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.IntegerField(source='lesson_set.all.count')
     lessons = LessonSerializer(source='lesson_set.all', many=True)
 
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
 
+    def get_is_subscribed(self, instance):
+        request = self.context.get('request')
+
+        if instance.subscription_set.filter(
+            user=request.user,
+            course=instance
+        ):
+            return True
+        return False
+
 
 class CourseCreateSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Course
         fields = '__all__'
