b22e1d51e16b5eff1b8e65f10c59ed50c738ae07
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-28 00:13:48 +0300

    создан PaymentCreateSerializer, изменен PaymentsSerializer

--- education/serializers.py
+++ education/serializers.py
@@ -1,7 +1,11 @@
+import stripe
+from django.conf import settings
 from rest_framework import serializers
 
 from education.models import Course, Lesson, Payments, Subscription
 from education.validators import LinkValidator
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
@@ -46,6 +50,40 @@ class CourseCreateSerializer(serializers.ModelSerializer):
 
 
 class PaymentsSerializer(serializers.ModelSerializer):
+    payment_data = serializers.SerializerMethodField()
+
     class Meta:
         model = Payments
         fields = '__all__'
+
+    def get_payment_data(self, instance):
+        if instance.stripe_id is not None:
+            return stripe.PaymentIntent.retrieve(instance.stripe_id)
+        elif instance.payment_method == 'transfer':
+            return ['Оплата была произведена переводом до подключения stripe']
+        else:
+            return ['Оплата наличными']
+
+
+class PaymentCreateSerializer(serializers.ModelSerializer):
+    payment_data = serializers.SerializerMethodField()
+    amount = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Payments
+        fields = '__all__'
+
+    def get_payment_data(self, instance):
+        if instance.payment_method == 'cash':
+            return ['Оплата наличными']
+        elif instance.payment_method == 'transfer':
+            pay = stripe.PaymentIntent.create(
+                amount=instance.amount,
+                currency='usd',
+                automatic_payment_methods={'enabled': True,
+                                           'allow_redirects': 'never'}
+            )
+            instance.stripe_id = pay['id']
+            instance.save()
+
+            return pay
